7a901f7b9ec7d0858d9920504e41ff56fc27babe
HanyHanya <yana2001sol@gmail.com>
2021-11-09 00:25:19 +0300

    больно

---
+++ Net.backup/Aircompany/Airport.cs
@@ -0,0 +1,64 @@
+﻿using Aircompany.Models;
+using Aircompany.Planes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aircompany
+{
+    public class Airport
+    {
+        public List<Plane> Planes { get; set; }
+
+        public Airport(IEnumerable<Plane> planes)
+        {
+            Planes = planes.ToList();
+        }
+
+        public IEnumerable<Plane> GetPlanes()
+        {
+            return Planes;
+        }
+
+        public List<PassengerPlane> GetPassengersPlanes()
+        {
+            return Planes.OfType<PassengerPlane>().ToList();
+        }
+
+        public List<MilitaryPlane> GetMilitaryPlanes()
+        {
+            return Planes.OfType<MilitaryPlane>().ToList();
+        }
+
+        public PassengerPlane GetPassengerPlaneWithMaxPassengersCapacity()
+        {
+            return GetPassengersPlanes().OrderByDescending(plane => plane.PassengersCapacity).First();
+        }
+        public List<MilitaryPlane> GetTransportMilitaryPlanes()
+        {
+            return Planes.OfType<MilitaryPlane>().Where(plane => plane.MilitaryPlaneType == MilitaryType.TRANSPORT).ToList();
+        }
+
+        public Airport SortPlanesByMaxDistance()
+        {
+            return new Airport(Planes.OrderBy(plane => plane.MaxFlightDistance));
+        }
+
+        public Airport SortPlanesByMaxSpeed()
+        {
+            return new Airport(Planes.OrderBy(plane => plane.MaxSpeed));
+        }
+
+        public Airport SortPlanesByMaxLoadCapacity()
+        {
+            return new Airport(Planes.OrderBy(plane => plane.MaxLoadCapacity));
+        }
+
+        public override string ToString()
+        {
+            return "Airport{" +
+                    "planes=" + string.Join(", ", Planes.Select(plane => plane.Model)) +
+                    '}';
+        }
+    }
+}
